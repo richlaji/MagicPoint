2a2c6ee171696b59c2204397a2180a5c01412a8f
richlaji <494846236@qq.com>
2017-11-06 10:23:17 +0800

    l

--- upload.py
+++ upload.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     os.system("git add *.py")
     print(2)
     print("git commit -m " + sys.argv[1])
-    os.system("git commit -m " + sys.argv[1])
+    os.system("git commit -m \"" + sys.argv[1] + "\"")
     print(3)
     os.system("git push")
